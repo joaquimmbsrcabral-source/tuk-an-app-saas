e0a710a12913444f27a852801e84e3e8e183d446
joaquimmbsrcabral-source <joaquimmbsrcabral@gmail.com>
2026-04-12 20:41:41 +0100

    feat: register service worker in main.tsx for push notifications
    Added service worker registration for push notifications.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { registerServiceWorker } from './lib/pushNotifications'
 import './i18n'
 import './index.css'
+
+// Register service worker for push notifications
+registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
